c286766043c6ed8e501f6e53ce3df34c8e3a999a
tunahan özkan <mhnds_tunahan@hotmail.com>
2023-07-28 14:09:59 +0300

    Booking_table_crud

---
+++ Casgem_CodeFirstProject/Controllers/BokingController.cs
@@ -0,0 +1,28 @@
+﻿using Casgem_CodeFirstProject.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Casgem_CodeFirstProject.Controllers
+{
+    public class BokingController : Controller
+    {
+        TravelContext travelContext = new TravelContext();
+        // GET: Boking
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var values = travelContext.Bookings.ToList();
+            return View(values);
+        }
+        public ActionResult DeleteBooking(int id)
+        {
+            var value = travelContext.Bookings.Find(id);
+            travelContext.Bookings.Remove(value);
+            travelContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
